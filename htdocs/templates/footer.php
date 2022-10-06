0ab93979cd2432e6e5df02ca136cd66532724e85
Rebecca Cohen <rcohen2@ufl.edu>
2022-10-06 12:35:12 -0400

    footer.php created

--- htdocs/templates/footer.php
+++ htdocs/templates/footer.php
@@ -1,3 +1,12 @@
+<?php
+    //Display general admin links
+    if((is_administrator()) || (isset($loggedin) && $loggedin)) {
+      // Create links to dsiplay
+      echo '<hr><h3>Site Admin</h3>
+      <p><a href="add_quote.php">Add Quote</a> | <a href="view_quote.php">View Quote</a> | <a href="logout.php">Logout</a></p>';
+    }
+    ?>
     </div>
+    <footer id="footer">Content &copy; <?php echo date('Y'); ?> </footer>
   </body>
 </html>
